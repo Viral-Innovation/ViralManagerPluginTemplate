7da70d5fc9925df5e23a98554b3c00ac9902aec8
AwesomeDude091 <computer.cubegeek4life@gmail.com>
2022-08-04 17:48:54 +0200

    Cleaner Template

--- src/main/java/com/viralinnovation/example/ExamplePlugin.java
+++ src/main/java/com/viralinnovation/example/ExamplePlugin.java
@@ -1,17 +1,9 @@
 package com.viralinnovation.example;
 
 import com.viralinnovation.viralmanager.api.plugins.Plugin;
-import com.viralinnovation.viralmanager.api.plugins.PluginConfig;
 import com.viralinnovation.viralmanager.api.plugins.Registry;
-import com.viralinnovation.viralmanager.api.plugins.exceptions.PluginException;
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
 
 public final class ExamplePlugin extends Plugin {
-    public ExamplePlugin(@NotNull PluginConfig pluginConfig, Logger logger) throws PluginException {
-        super(pluginConfig, logger);
-        logger.info("YAY! This shit works");
-    }
 
     @Override
     public void register(Registry registry) {
@@ -19,7 +11,7 @@ public final class ExamplePlugin extends Plugin {
     }
 
     @Override
-    public void deregister() {
+    public void disable() {
 
     }
 }
